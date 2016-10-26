93539f2528d29c53268b66dbba5952a59ed56c33
sateeshb2 <sateesh.aws@gmail.com>
2016-10-26 18:31:45 -0400

    Update site.pp

--- site.pp
+++ site.pp
@@ -1,1 +1,22 @@
+node '*' {  
+  class { 'apache':    
+    default_mods => false, 
+  }  
 
+  include apache::mod::ssl  
+
+  apache::vhost { '*-http':    
+    servername      => 'sateesh.puppetagent*.com',    
+    port            => '80',    
+    docroot         => '/var/www/html/',    
+    redirect_status => 'permanent',    
+    redirect_dest   => 'https://sateesh.puppetagent*.com/',  
+  }  
+
+  apache::vhost { '*-https':    
+    servername      => 'sateesh.puppetagent*.com',    
+    port            => '443',    
+    docroot         => '/var/www/html/',    
+    ssl             => true,    
+  }
+}
